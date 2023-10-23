4f5e823e9214043ac7af30df91901d4cfd8eaa7c
Ryou1026 <r.koyano1026@gmail.com>
2023-10-23 17:17:46 +0900

    一定範囲内のメモを返す

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -4,21 +4,14 @@ namespace App\Http\Controllers;
 
 class MemoController extends Controller
 {
-    public function index($x, $y)
+    public function index(float $x, float $y)
     {
-        // $x = $x;
-        // $y = $y;
-        // $point1 = \DB::select('select ST_GeomFromText(\'POINT(139.76 35.27)\', 4326)');
-        // $point2 = \DB::select('select ST_GeomFromText(\'POINT(139.75 35.69)\', 4326)');
 
-        // // point1 ... ユーザの座標　　point2 ... メモの座標
-        // $distance = \DB::select('select ST_DistanceSphere(:point1、:point2) from memos');
-        // $raw = 'select * from memos where $distance <= 50';
-        // return view('memo', ['distance' => $distance]);
+        $distance = \DB::table('memos')
+            ->select(\DB::raw('*, ST_Distance(\'SRID=4326;POINT('.$x.' '.$y.')\', memos."memoCoordinate")'))
+            ->whereRaw('ST_Distance(\'SRID=4326;POINT('.$x.' '.$y.')\', memos."memoCoordinate") < 1000')
+            ->limit(10)->get();
 
-        $distance = \DB::select('select * from memos');
-
-        // where ST_Distance(ST_GeomFromText(\'POINT(139.1235 69.3521)\',4326), ST_GeomFromText(\'POINT(139.1235 35.3521)\',4326)) <= 50
         return response()->json($distance);
     }
 }
