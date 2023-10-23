2406c748d52abb9e4faa78698fb9515a873e2e42
Ryou1026 <r.koyano1026@gmail.com>
2023-10-23 15:36:53 +0900

    DB内容取得

---
+++ app/Http/Controllers/MemoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemoController extends Controller
+{
+    public function index($x, $y)
+    {
+        // $x = $x;
+        // $y = $y;
+        // $point1 = \DB::select('select ST_GeomFromText(\'POINT(139.76 35.27)\', 4326)');
+        // $point2 = \DB::select('select ST_GeomFromText(\'POINT(139.75 35.69)\', 4326)');
+
+        // // point1 ... ユーザの座標　　point2 ... メモの座標
+        // $distance = \DB::select('select ST_DistanceSphere(:point1、:point2) from memos');
+        // $raw = 'select * from memos where $distance <= 50';
+        // return view('memo', ['distance' => $distance]);
+
+        $distance = \DB::select('select * from memos');
+        // where ST_Distance(ST_GeomFromText(\'POINT(139.1235 69.3521)\',4326), ST_GeomFromText(\'POINT(139.1235 35.3521)\',4326)) <= 50
+        return response()->json($distance);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test_get/{x}/{y}', [MemoController::class, 'index']);
