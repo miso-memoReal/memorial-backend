0006d66ef266be77eb397c9918ac258cc1da4adb
na2na-p <na2na@na2na.dev>
2023-10-23 20:58:14 +0900

    Refactored create-testing-database.sql to remove unnecessary new line character.

--- infra/postgres/20-create-testing-database.sql
+++ infra/postgres/20-create-testing-database.sql
@@ -1,6 +1,2 @@
 SELECT 'CREATE DATABASE testing'
-WHERE NOT EXISTS (
-        SELECT
-        FROM pg_database
-        WHERE datname = 'testing'
-    ) \ gexec
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testing')\gexec
