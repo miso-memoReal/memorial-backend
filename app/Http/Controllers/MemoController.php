015443a84b579ff25ac61705b7f583d3650fa34d
Ryou1026 <r.koyano1026@gmail.com>
2023-10-27 12:07:32 +0900

    変数名修正

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,13 +7,19 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    public function index(float $x, float $y)
+    public function index(float $longitude, float $latitude)
     {
-
         $distance = \DB::table('memos')
-            ->select(\DB::raw('"id", "content", ST_Distance(\'SRID=4326;POINT(' . $x . ' ' . $y . ')\', memos."coordinate")'))
-            ->whereRaw('ST_Distance(\'SRID=4326;POINT(' . $x . ' ' . $y . ')\', memos."coordinate") < 50')
+            ->select(
+                \DB::raw('"id", "content", ST_Distance(\'SRID=4326;POINT(' . $longitude . ' ' . $latitude . ')\', memos."coordinate")'),
+                \DB::raw('ST_X(ST_AsText(memos."coordinate")) as longitude'),
+                \DB::raw('ST_Y(ST_AsText(memos."coordinate")) as latitude'),
+                \DB::raw('ST_Distance(ST_MakePoint(' . $latitude . ', ' . $longitude . ')::geography, memos."coordinate")')
+            )
+            ->whereRaw('ST_Distance(\'SRID=4326;POINT(' . $longitude . ' ' . $latitude . ')\', memos."coordinate") < 50')
             ->limit(10)->get();
+
+
 
         return response()->json($distance);
     }
