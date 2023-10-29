3b4afad39b8753df64e7d7817c1d425d1bb810e6
na2na-p <na2na@na2na.dev>
2023-10-29 12:39:04 +0900

    セルフレビュー事項修正
    - 一部不変の値の定数化
    - importで扱える場所はそうする

--- app/Http/Services/MemoService.php
+++ app/Http/Services/MemoService.php
@@ -3,7 +3,13 @@
 namespace App\Http\Services;
 
 use App\Http\ValueObjects\Coordinate;
+use DB;
 use Illuminate\Support\Collection;
+
+// 距離閾値
+const DISTANCE_THRESHOLD = 50;
+// 件数上限
+const LIMIT = 10;
 
 class MemoService
 {
@@ -14,11 +20,11 @@ class MemoService
     {
         $point = $coordinate->toPoint();
 
-        return \DB::table('memos')
-            ->select(['id', 'content', \DB::raw("ST_Distance('{$point}', memos.\"coordinate\") as distance")])
-            ->whereRaw('ST_Distance('.\DB::getPdo()->quote($point).', memos."coordinate") < 50')
-            ->orderBy('distance', 'asc')
-            ->limit(10)
+        return DB::table('memos')
+            ->select(['id', 'content', DB::raw("ST_Distance('{$point}', memos.\"coordinate\") as distance")])
+            ->whereRaw('ST_Distance('. DB::getPdo()->quote($point).', memos."coordinate") < '.DISTANCE_THRESHOLD)
+            ->orderBy('distance')
+            ->limit(LIMIT)
             ->get();
     }
 }
